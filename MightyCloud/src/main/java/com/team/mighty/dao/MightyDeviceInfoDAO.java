90652f0a081b1735055ac61c8b6732d8b8185146
shankar narayanan <shankar narayanan>
2016-09-07 00:26:29 +0800

    validate device

--- MightyCloud/src/main/java/com/team/mighty/dao/MightyDeviceInfoDAO.java
+++ MightyCloud/src/main/java/com/team/mighty/dao/MightyDeviceInfoDAO.java
@@ -8,6 +8,6 @@ import com.team.mighty.domain.MightyDeviceInfo;
 
 public interface MightyDeviceInfoDAO extends JpaRepository<MightyDeviceInfo, Long> {
 
-	@Query("SELECT deviceId FROM MightyDeviceInfo WHERE deviceId = :deviceId")
+	@Query("SELECT m FROM MightyDeviceInfo m WHERE m.deviceId = :deviceId")
 	MightyDeviceInfo getDeviceInfo(@Param("deviceId") String deviceId);
 }
